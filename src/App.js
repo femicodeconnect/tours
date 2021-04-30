8602d5e90a00fbdd21884f0077712aa199e7bfe7
Femi <ologunwafemi@gmail.com>
2021-04-30 01:12:16 +0100

    fetched tours from api using fetchAPI (async/await)

--- src/App.js
+++ src/App.js
@@ -8,6 +8,28 @@ function App() {
    const [loading, setLoading] = useState(true);
    const [tours, setTours] = useState([]);
 
+   //function to fetch tours
+   const fetchTours = async () => {
+      //an extra precaution to ensure that loading is truw while fetching data
+      setLoading(true);
+
+      try {
+         const response = await fetch(url);
+         const tours = await response.json();
+         //console.log(tours);
+         setLoading(false);
+         setTours(tours);
+      } catch (error) {
+         setLoading(false);
+         console.log(error);
+      }
+   };
+
+   //calling fetchTours function when page loads
+   useEffect(() => {
+      fetchTours();
+   }, []);
+
    if (loading) {
       return (
          <main>
