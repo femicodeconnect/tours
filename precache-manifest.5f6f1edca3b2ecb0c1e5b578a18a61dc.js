7544b383831b827983ed93404bfd5b1173dd5c89
Femi <femiconnect234@gmail.com>
2021-05-16 07:17:15 +0100

    Updates

---
+++ precache-manifest.5f6f1edca3b2ecb0c1e5b578a18a61dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f47abb28365c14ebc77066191cc5b56",
+    "url": "/tours/index.html"
+  },
+  {
+    "revision": "2f6d21378eb40f9adb7b",
+    "url": "/tours/static/css/main.8e49c9d7.chunk.css"
+  },
+  {
+    "revision": "367d3ad82c25432129b5",
+    "url": "/tours/static/js/2.0bb548be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tours/static/js/2.0bb548be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f6d21378eb40f9adb7b",
+    "url": "/tours/static/js/main.7a19aeea.chunk.js"
+  },
+  {
+    "revision": "48ca4900ea6e6503a933",
+    "url": "/tours/static/js/runtime-main.c4302faf.js"
+  }
+]);
